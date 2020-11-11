d3594a4eb8941457d8457953fab435e94665a738
wasmenezes <was288519@gmail.com>
2020-11-10 22:53:39 -0300

    move error 401 to a helper method

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -20,8 +20,6 @@ module.exports = class LoginRouter {
 
     this.authUseCase.auth(email, password)
 
-    return {
-      statusCode: 401
-    }
+    return HttpResponse.unauthorizedError()
   }
 }
