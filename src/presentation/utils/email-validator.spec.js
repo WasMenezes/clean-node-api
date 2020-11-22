dfa227a943d0620f23d0c7899193da78797e5015
wasmenezes <was288519@gmail.com>
2020-11-22 16:31:27 -0300

    ensure EmailValidator calls validator with correct email

--- src/presentation/utils/email-validator.spec.js
+++ src/presentation/utils/email-validator.spec.js
@@ -11,16 +11,22 @@ const makeSut = () => {
 }
 
 describe('Email Validator', () => {
-  test('Should return true if validator returns true ', () => {
+  test('Should return true if validator returns true', () => {
     const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@email.com')
     expect(isEmailValid).toBe(true)
   })
 
-  test('Should return false if validator returns false ', () => {
+  test('Should return false if validator returns false', () => {
     validator.isEmailValid = false
     const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_email@email.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = makeSut()
+    sut.isValid('invalid_email@email.com')
+    expect(validator.email).toBe('invalid_email@email.com')
   })
 })
