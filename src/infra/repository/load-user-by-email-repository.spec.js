a092385cd5a1d7bdf7754178845a9fb9efa0cf90
wasmenezes <was288519@gmail.com>
2020-11-30 16:58:45 -0300

    ensure LoadUserByEmailRepository returns null if no user is found

---
+++ src/infra/repository/load-user-by-email-repository.spec.js
@@ -0,0 +1,12 @@
+class LoadUserByEmailRepository {
+  async load (email) {
+    return null
+  }
+}
+describe('LoadUserByEmail ', () => {
+  test('Should return null if no user is found ', async () => {
+    const sut = new LoadUserByEmailRepository()
+    const user = await sut.load('invalid_email@email.com')
+    expect(user).toBeNull()
+  })
+})
