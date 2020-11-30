2e6e03d3460c269405d2e456c691a21644236720
wasmenezes <was288519@gmail.com>
2020-11-30 12:59:59 -0300

    ensure TokenGenerator returns null if jwt returns null

---
+++ src/utils/helpers/token-generator.spec.js
@@ -0,0 +1,12 @@
+class TokenGenerator {
+  async generate (id) {
+    return null
+  }
+}
+describe('should ', () => {
+  test('Should return null if JWT returns null ', async () => {
+    const sut = new TokenGenerator()
+    const token = await sut.generate('any_id')
+    expect(token).toBeNull()
+  })
+})
