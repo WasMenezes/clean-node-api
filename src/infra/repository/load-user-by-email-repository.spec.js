87a46bb17841a0bd23eaffd88ed49a673b212e46
Waschington Luiz Menezes <wmenezes@elfsm.local>
2020-12-01 13:23:23 -0300

    ensure LoadUserByEmailRepository throws if no userModel is provided

--- src/infra/repository/load-user-by-email-repository.spec.js
+++ src/infra/repository/load-user-by-email-repository.spec.js
@@ -26,7 +26,7 @@ describe('LoadUserByEmail ', () => {
     await MongoHelper.close()
   })
 
-  test('Should return null if dno user is found ', async () => {
+  test('Should return null if no user is found ', async () => {
     const { sut } = makeSut()
     const user = await sut.load('invalid_email@email.com')
     expect(user).toBeNull()
@@ -46,5 +46,11 @@ describe('LoadUserByEmail ', () => {
       _id: fakeUser.ops[0]._id,
       password: fakeUser.ops[0].password
     })
+  })
+
+  test('Should throw if no userModel is provided', async () => {
+    const sut = new LoadUserByEmailRepository()
+    const promise = sut.load('any_email@email.com')
+    expect(promise).rejects.toThrow()
   })
 })
